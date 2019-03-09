b574e79e1beb98cf35f93dc68b54076cb0d9a9dd
Lukáš Bubník <lukas.bu@seznam.cz>
2019-03-09 21:53:33 +0100

    MainMenu refactors

---
+++ Tetris/ViewModels/MainMenuViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using Tetris.Views;
+
+namespace Tetris.ViewModels
+{
+    public class MainMenuViewModel
+    {
+        #region Properties
+        
+        public MenuManager MenuManager { get; set; }
+
+        #endregion Properties
+
+        #region Constructor
+        
+        public MainMenuViewModel(MainMenuView mainMenuView, TextBlock[] menuItems)
+        {
+            var highlightColor = (SolidColorBrush)Application.Current.Resources["MainMenuItemHighlightBrush"];
+            MenuManager = new MenuManager(mainMenuView, menuItems, highlightColor);
+        }
+
+        #endregion Constructor
+    }
+}
---
+++ Tetris/Views/MainMenuView.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Tetris.ViewModels;
+
+namespace Tetris.Views
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class MainMenuView
+    {
+        #region Properties
+
+        public MainMenuViewModel ViewModel
+        {
+            get => DataContext as MainMenuViewModel;
+            set => DataContext = value;
+        }
+
+        #endregion Properties
+
+        #region Constructor
+        
+        public MainMenuView()
+        {
+            InitializeComponent();
+            var menuItems = new [] {
+                StartTextBlock,
+                ScoreTextBlock,
+                ExitTextBlock
+            };
+
+            ViewModel = new MainMenuViewModel(this, menuItems);
+        }
+
+        #endregion Constructor
+    }
+}
